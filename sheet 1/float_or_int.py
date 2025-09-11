058b0ddb0f04e2bc367538dd391ffca8cc8df580
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-09-11 20:53:56 +0300

    check if the number float or int

---
+++ sheet 1/float_or_int.py
@@ -0,0 +1,10 @@
+N = input().strip()
+
+if "." in N:  # has decimal point
+    int_part, dec_part = N.split(".")
+    if int(dec_part) == 0:  # decimal part is all zeros
+        print("int", int(int_part))
+    else:
+        print("float", int(int_part), "0." + dec_part)
+else:  # pure integer
+    print("int", int(N))
