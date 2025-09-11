f226ed0cc364033e6c2c9864a78c91634c12e93d
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-09-11 20:24:54 +0300

    resolve the line and make the operation if true return true if not return the true value

---
+++ sheet 1/mathematical_expression.py
@@ -0,0 +1,23 @@
+import os
+# line=input().split()
+# op,nums=line[1],[int(line[0]),int(line[2]),int(line[4])]
+
+# match(op):
+#     case "+":
+#         if nums[0] + nums[1]==nums[2]:
+#             print("Yes")
+#         else:
+#             print(nums[0]+nums[1])
+#     case "-":
+#         if nums[0] - nums[1]==nums[2]:
+#             print("Yes")
+#         else:
+#             print(nums[0]-nums[1])
+#     case "*":
+#         if nums[0] * nums[1]==nums[2]:
+#             print("Yes")
+#         else:
+#             print(nums[0]*nums[1])
+    
+os.path     
+    
