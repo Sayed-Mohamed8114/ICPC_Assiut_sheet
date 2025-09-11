71d4581327e3e161e58231ea565443b5d024b245
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-09-11 20:32:29 +0300

    the right value or yes

--- sheet 1/mathematical_expression.py
+++ sheet 1/mathematical_expression.py
@@ -1,23 +1,20 @@
-import os
-# line=input().split()
-# op,nums=line[1],[int(line[0]),int(line[2]),int(line[4])]
+line=input().split()
+op,nums=line[1],[int(line[0]),int(line[2]),int(line[4])]
 
-# match(op):
-#     case "+":
-#         if nums[0] + nums[1]==nums[2]:
-#             print("Yes")
-#         else:
-#             print(nums[0]+nums[1])
-#     case "-":
-#         if nums[0] - nums[1]==nums[2]:
-#             print("Yes")
-#         else:
-#             print(nums[0]-nums[1])
-#     case "*":
-#         if nums[0] * nums[1]==nums[2]:
-#             print("Yes")
-#         else:
-#             print(nums[0]*nums[1])
-    
-os.path     
-    
+match(op):
+    case "+":
+        if nums[0] + nums[1]==nums[2]:
+            print("Yes")
+        else:
+            print(nums[0]+nums[1])
+    case "-":
+        if nums[0] - nums[1]==nums[2]:
+            print("Yes")
+        else:
+            print(nums[0]-nums[1])
+    case "*":
+        if nums[0] * nums[1]==nums[2]:
+            print("Yes")
+        else:
+            print(nums[0]*nums[1])
+     
