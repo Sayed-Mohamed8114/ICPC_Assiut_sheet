166682d29b0d1fb7137f9a59b3d22a40a1cca0ca
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-09-17 21:47:16 +0300

    evens to num

--- sheet2/even_numbers.py
+++ sheet2/even_numbers.py
@@ -1,7 +1,16 @@
 x=int(input())
-
+evens=[]
 for i in range(1,x+1):
     if i %2==0:
-        print(i)
-    else:
-        continue
+        evens.append(i)
+
+if not evens:
+    print(-1)
+else:
+    for i in range(len(evens)):
+        print(evens[i])
+
+
+
+
+        
